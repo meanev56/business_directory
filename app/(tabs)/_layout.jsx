91a1d900a1ae17db0ea2bd1edb31ee134b3950d5
meanev56 <emekanjoku86@gmail.com>
2025-12-31 06:12:53 +0100

    added -tabs layout screens

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,42 @@
 import { Tabs } from 'expo-router/tabs';
+import { Colors } from '../../constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
    return (
     <Tabs
-     
+      screenOptions={{
+         headerShown: false,
+         tabBarActiveTintColor: Colors.PRIMARY,
+      }}
     >
-
+      <Tabs.Screen
+         name="home"
+         options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color }) => (
+               <Ionicons name="home" size={24} color={color} />
+            ),
+         }}
+      />
+      <Tabs.Screen
+         name="explore"
+         options={{
+            tabBarLabel: "Explore",
+            tabBarIcon: ({ color }) => (
+               <Ionicons name="search" size={24} color={color} />
+            ),
+         }}
+      />
+      <Tabs.Screen
+         name="profile"
+         options={{
+            tabBarLabel: "Profile",
+            tabBarIcon: ({ color }) => (
+               <Ionicons name="people-circle" size={24} color={color} />
+            ),
+         }}
+      />
     </Tabs>
    ) 
 }

7e969bdb7fb9287d10d4fda79fb1e6260410c6ab
meanev56 <emekanjoku86@gmail.com>
2025-12-31 02:49:30 +0100

    linked index to home

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,33 @@
-import { ClerkProvider } from "@clerk/clerk-expo";
-import { Slot } from 'expo-router';
-import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import LoginScreen from "./components/LoginScreen";
+import * as SecureStore from 'expo-secure-store';
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      const item = await SecureStore.getItemAsync(key);
+      if (item) {
+        console.log(`${key} was used 🔐 \n`);
+      } else {
+        console.log("No values stored under key: " + key);
+      }
+      return item;
+    } catch (error) {
+      console.error("SecureStore get item error: ", error);
+      await SecureStore.deleteItemAsync(key);
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 
 export default function RootLayout() {
@@ -15,7 +42,15 @@ export default function RootLayout() {
       tokenCache={tokenCache} 
       publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY    
      }>
-      <Slot />
+      <SignedIn>
+        <Stack screenOptions={{ headerShown: false}}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </SignedIn>
+
+      <SignedOut>
+        <LoginScreen />
+      </SignedOut>
     </ClerkProvider>
   )
 }
